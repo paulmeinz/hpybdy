8990d383b185c82771e7e30295c89d1a79f5778e
Paul Meinz <paulmeinz@gmail.com>
2015-12-01 15:40:21 -0800

    feat: add wish_happy_bday() function and documentation

---
+++ R/wish_happy_bdy.R
@@ -0,0 +1,28 @@
+#' Wish happy birthday.
+#'
+#' Generates a plot wishing a specific individual happy birthday.
+#'
+#' @param name A character vector of length 1
+#'
+#' @section Warning:
+#' This function does not yet support names with spaces
+
+wish_happy_bday <- function(name) {
+
+  if (!is.character(name)) {
+    stop('name must be a character')
+  }
+
+  if (length(name) > 1 || length(name) == 0) {
+    stop('character vector must be of length 1 exactly')
+  }
+
+  plot.new()
+  msg <- determine_letter_indx('happybday')
+  text(1:length(msg)/length(msg)-.1,.7, labels = letters[msg],
+       col = sample(1:100, 9, replace = TRUE), cex = 2)
+
+  indexes <- determine_letter_indx(name)
+  text(1:length(indexes)/length(indexes)-.1,.4, labels = letters[indexes],
+       col = sample(1:100, length(indexes), replace = TRUE), cex = 3)
+}
