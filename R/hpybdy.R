77fe847bcd1e3c25612221433d1b4c9ac911036d
Paul Meinz <paulmeinz@gmail.com>
2015-12-01 15:16:21 -0800

    feat: added package documentation and added determine_letter_indx.Rd

---
+++ R/hpybdy.R
@@ -0,0 +1,15 @@
+#' hpybdy: A set of functions for wishing someone happy birthday, nerd style
+#'
+#' The hpybdy package provides a function and set of base functions for
+#' wishing someone special a happy birthday.
+#'
+#' @section Functions:
+#' determine_letter_index() - takes a character vector of length 1 and determines
+#' the index number of each letter in the character in the letters object.
+#'
+#' wish_happy_bday() - generates a plot that wishes someone happy bday in festive
+#' colors.
+#'
+#' @docType package
+#' @name hpybdy
+NULL
